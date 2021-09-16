2739d3a1b1a3215d6a3bb187e8180901ff7908ee
CameronC <cameroncastor05@gmail.com>
2021-09-15 21:16:47 -0600

    added first vars

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
+var numberChar =  "0123456789";
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength;
+
+
+
 
 // Write password to the #password input
 function writePassword() {
