7e8a9d95eb64b5dc3056c8eb80d5f69e4ff89045
CameronC <cameroncastor05@gmail.com>
2021-10-05 02:05:47 -0600

    added comments

--- script.js
+++ script.js
@@ -10,31 +10,35 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var number = "0123456789"
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-var special = "*&^%$#@!?><{}"
+// chars that are available to use 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
+var number = "0123456789" .split("");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz" .split("");
+var special = "*&^%$#@!?><{}" .split("");
 var collectionOfValids = [];
 
 function generatePassword () {
+  // prompt to choose length of password
   var passwordLength = prompt("Enter how many characters you would like your password to be 8-128 characters.");
 
+  var lengthConfirm = parseInt(passwordLength);
+  console.log(lengthConfirm);
+// gives option of how manu characters you wanna use
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert ("error. Choice is not valid try again.");
     return;
   }
-}
 
-var UpperCaseL = prompt("Do you want your password to have uppercase letters?");
+// uppercase prompt
+var UpperCaseL = confirm("Do you want your password to have uppercase letters?");
 if (UpperCaseL === true) {
   for (var i = 0; i < upperCase.length; i++) {
     collectionOfValids.push(upperCase[i]);
     
   }
 }
-
-var lowerCaseL = prompt("Do you want your password to have lowercase letters?");
+// lowercase prompt
+var lowerCaseL = confirm("Do you want your password to have lowercase letters?");
 if (lowerCaseL === true) {
   for (var i = 0; i < lowerCase.length; i++) {
     collectionOfValids.push(lowerCase[i]);
@@ -42,20 +46,34 @@ if (lowerCaseL === true) {
 
   }
 }
-
-var numberChar = prompt("Do you want to include numbers in your password?");
+// number promt
+var numberChar = confirm("Do you want to include numbers in your password?");
 if (numberChar === true) {
   for (var i = 0; i < number.length; i++) {
     collectionOfValids.push(number[i]);
   }
 }
-
-var specialChar = prompt("Do you want to include Special letters in your password?");
+// special character promt
+var specialChar = confirm("Do you want to include Special letters in your password?");
 if (specialChar === true) {
   for (var i = 0; i < special.length; i++) {
     collectionOfValids.push(special[i]);
   }
 }
+// creat a random passowrd 
+var randomPass = "";
+for (var i = 0; i < lengthConfirm; i++) {
+  collectionOfValids[
+    Math.floor(Math.random() * collectionOfValids.length)];
+    randomPass += 
+    collectionOfValids[
+      Math.floor(Math.random() * collectionOfValids.length)
+    ];
+  
+}
+return randomPass;
+}
+
 
 
 // Add event listener to generate button
