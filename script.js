abbb6f82cf6525acbab894f0cf8d0f2ad3f8b68b
CameronC <cameroncastor05@gmail.com>
2021-10-05 01:20:43 -0600

    added prompt for numbers and lowercase

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var number = "0123456789"
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var special = "*&^%$#@!?><{}"
+var collectionOfValids = [];
 
 function generatePassword () {
   var passwordLength = prompt("Enter how many characters you would like your password to be 8-128 characters.");
@@ -24,6 +25,31 @@ function generatePassword () {
     return;
   }
 }
+
+var UpperCaseL = prompt("Do you want your password to have uppercase letters?");
+if (UpperCaseL === true) {
+  for (var i = 0; i < upperCase.length; i++) {
+    collectionOfValids.push(upperCase[i]);
+    
+  }
+}
+
+var lowerCaseL = prompt("Do you want your password to have lowercase letters?");
+if (lowerCaseL === true) {
+  for (var i = 0; i < lowerCase.length; i++) {
+    collectionOfValids.push(lowerCase[i]);
+
+
+  }
+}
+
+var numberChar = prompt("Do you want to include numbers in your password?");
+if (numberChar === true) {
+  for (var i = 0; i < number.length; i++) {
+    collectionOfValids.push(number[i]);
+  }
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
